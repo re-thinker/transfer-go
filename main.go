2e2282d2e7f0b661046ca6b7936011b76ae5bfa7
re-thinker <re-thinker@gmail.com>
2018-07-13 20:01:49 +0800

    add transfer

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
 package main
 
+import (
+	"convert"
+	"fmt"
+	"transfer"
+)
 
+func main()  {
+	by := []byte{0x00, 0x00, 0x03, 0xe8}
+	var num int32
+	convert.Bytetoint(by, &num)
+	fmt.Println("num :", num)
+
+	by2 := []byte{}
+	var num2 int32
+	num2 = 333
+	by2 = convert.Inttobyte(&num2)
+	fmt.Println(by2)
+
+	var num3 int32
+	convert.Bytetoint(by2, &num3)
+	fmt.Println(num3)
+
+	tcp := transfer.TCPTransfer{}
+	tcp.Init()
+	buf := make([]byte, 10)
+	tcp.Read(buf)
+	fmt.Println(buf)
+
+}
